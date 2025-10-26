be848fa38d169f21cfe65e262e7c22c569609ece
Manojkumar <manojkumar18102006@gmail.com>
2025-10-26 21:49:40 +0530

    initialize the spring boot project

---
+++ StudySessionPlanner/StudySessionPlanner/src/main/java/com/example/StudySessionPlanner/StudySessionPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudySessionPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySessionPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySessionPlannerApplication.class, args);
+	}
+
+}
---
+++ StudySessionPlanner/StudySessionPlanner/src/test/java/com/example/StudySessionPlanner/StudySessionPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudySessionPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySessionPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
